bddcaa6c3eb6cd7cc3a5eda15c4f7ff4dde4740d
Andre Cruz <andreldcruz@outlook.com>
2022-10-13 20:20:29 -0300

    Teste de variavel de ambiente com endereço da API funcionando

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { DriverTable, MRData, result } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
-  private readonly url = 'http://ergast.com/api/f1/2022/drivers.json';
+  private readonly url = `${environment.API}/2022/drivers.json`;
 
   constructor(private http: HttpClient) {}
 
