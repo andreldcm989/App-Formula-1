39f4b2713ba74569cfd0b6081fe7c8f6de919349
Andre Cruz <andreldcruz@outlook.com>
2022-10-14 15:31:33 -0300

    Atualização das variaveis de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
   API: 'http://ergast.com/api/f1',
+  RAPIDAPI_F1: 'https://api-formula-1.p.rapidapi.com',
+  KEY: {
+    'x-rapidapi-key': 'f106352d4emsh6cd59fabb51059dp1ec488jsn218d1cef7a11',
+  },
+  HOST: { 'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com' },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,11 @@
 export const environment = {
   production: false,
   API: 'http://ergast.com/api/f1',
+  RAPIDAPI_F1: 'https://api-formula-1.p.rapidapi.com',
+  KEY: {
+    'x-rapidapi-key': 'f106352d4emsh6cd59fabb51059dp1ec488jsn218d1cef7a11',
+  },
+  HOST: { 'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com' },
 };
 
 /*
